ae59fc30e179390c47a62a20a4d668e3927ec7f8
mukundmotwani <mukund4motwani@gmail.com>
2024-02-28 20:59:08 +0530

    Initial commit

---
+++ src/test/java/technogise/demo/order/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package technogise.demo.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
