69a8e12d615e9ad96116f25b7719948880f78ac8
mukundmotwani <mukund4motwani@gmail.com>
2024-02-29 21:19:47 +0530

    Initial setup-classes

--- order/src/main/java/technogise/demo/order/controller/OrderController.java
+++ order/src/main/java/technogise/demo/order/controller/OrderController.java
@@ -1,19 +1,23 @@
 package technogise.demo.order.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import technogise.demo.order.client.InventoryClient;
+import technogise.demo.order.service.OrderService;
 
 @RestController
 public class OrderController {
 
     @Autowired
-    InventoryClient inventoryClient;
+    OrderService orderService;
 
     @GetMapping("/check")
-    public boolean checkSetup()
-    {
-        return inventoryClient.isInventoryAvailable();
+    public ResponseEntity checkSetup() {
+        boolean orderCreated = orderService.createOrder();
+        HttpStatus status = orderCreated ? HttpStatus.CREATED : HttpStatus.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(status).build();
     }
 }
---
+++ order/src/main/java/technogise/demo/order/service/OrderService.java
@@ -0,0 +1,22 @@
+package technogise.demo.order.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import technogise.demo.order.client.InventoryClient;
+import technogise.demo.order.client.ShippingClient;
+
+@Service
+public class OrderService {
+    @Autowired
+    private InventoryClient inventoryClient;
+    @Autowired
+    private ShippingClient shippingClient;
+
+    public boolean createOrder() {
+        if (!inventoryClient.isInventoryAvailable() || !shippingClient.isItemShippable()) {
+            return false;
+        }
+        //Persist to db
+        //Call Notification service        return true;
+    }
+}
